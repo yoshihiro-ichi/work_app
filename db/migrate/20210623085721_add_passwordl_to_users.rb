522c04b12ae80be2b2b62451d618a6156b14dd1f
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-06-24 02:24:21 +0000

    ワーク課題

---
+++ db/migrate/20210623085721_add_passwordl_to_users.rb
@@ -0,0 +1,5 @@
+class AddPasswordlToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :password, :string
+  end
+end
---
+++ db/migrate/20210623090418_rename_from_password_tot_password_digest_on_users.rb
@@ -0,0 +1,4 @@
+class RenameFromPasswordTotPasswordDigestOnUsers < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
